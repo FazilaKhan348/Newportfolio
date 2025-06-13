dc88deb091199400111dc54a7d642ada02fb58e0
FazilaKhan348 <fazeelak690@gmail.com>
2025-06-14 04:21:57 +0500

    Initial portfolio upload

---
+++ Assets/main.js
@@ -0,0 +1,28 @@
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+
+
+ window.onscroll = function () {
+    const scrollBtn = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollBtn.style.display = "block";
+    } else {
+      scrollBtn.style.display = "none";
+    }
+  };
+
+  // Scroll to top when button clicked
+  document.getElementById("scrollTopBtn").onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
